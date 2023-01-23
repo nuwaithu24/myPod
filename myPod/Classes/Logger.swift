04bed5e89692eb2047f4d1fe4c57ba80f1fb6b56
nuwaithu24 <nuwaithu90@gmail.com>
2023-01-23 17:56:27 +0900

    push

---
+++ myPod/Classes/Logger.swift
@@ -0,0 +1,18 @@
+//
+//  Logger.swift
+//  myPod
+//
+//  Created by Nu Wai Thu on 2023/01/23.
+//
+
+import Foundation
+
+public class Logger{
+    
+    public init(){
+        
+    }
+    public func ouputString(text:String){
+        print(text)
+    }
+}
